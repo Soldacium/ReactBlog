42ace55d5b64ce81a4d3ea623b3c7442360b5ef6
Soldacium <laurentyfenix@gmail.com>
2020-11-21 10:23:08 +0100

    initial commit

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+//import { persistReducer } from 'redux-persist'
+//import storage from 'redux-persist/lib/storage'; //for window storage, another for session storage
+
+const rootReducer = combineReducers({
+
+})
+
+export default rootReducer
